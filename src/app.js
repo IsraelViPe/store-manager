612b1230fef49832e95983e05905bc7f6c3d9c49
IsraelPereira <pereira.israel2070@gmail.com>
2023-01-14 00:12:55 -0300

    feat: adiciona o connection na camada model, prepara todo o setup, cria collection no tunderClient

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const connection = require('./models/connection');
 
 const app = express();
 
@@ -7,7 +8,14 @@ app.get('/', (_request, response) => {
   response.send();
 });
 
+app.get('/products', async (_req, res) => {
+  const [result] = await connection.execute(
+    'SELECT * FROM StoreManager.products;',
+  );
+  res.status(200).json(result);
+});
+
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação 
+// você deve usar o arquivo index.js para executar sua aplicação
 module.exports = app;
