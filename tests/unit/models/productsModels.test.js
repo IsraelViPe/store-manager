94a33e30e3d3dab1358ec5c70689024539f884bb
IsraelPereira <pereira.israel2070@gmail.com>
2023-01-14 00:47:26 -0300

    feat: implementa testes da função findAll /model/products e depois implementa a função na model

--- tests/unit/models/productsModels.test.js
+++ tests/unit/models/productsModels.test.js
@@ -1,3 +1,24 @@
 const sinon = require('sinon');
 const { expect } = require('chai');
+const connection = require('../../../src/models/connection');
 
+const { productsModel } = require('../../../src/models');
+
+const { productsList } = require('./productsModel.mock');
+
+describe('PRODUCTS MODEL', function () {
+  afterEach(function () {
+    sinon.restore();
+  });
+  it('se retorna a lista de todos os produtos corretamente', async function () {
+    sinon.stub(connection, 'execute').resolves([productsList]);
+    const result = await productsModel.findAll();
+    expect(result).to.be.deep.equal(productsList);
+  })
+  it('se é possível buscar um produto por id', async function () {
+    sinon.stub(connection, 'execute').resolves([productsList[0]]);
+    const result = await productsModel.findById(1);
+    expect(result).to.be.deep.equal(productsList[0])
+  })
+
+})
