daf3506259c741891f559f5b4d7389e18e1b023d
IsraelPereira <pereira.israel2070@gmail.com>
2023-01-24 05:35:46 -0300

    delete: limpando react-app

--- src/app.js
+++ src/app.js
@@ -9,7 +9,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
@@ -30,13 +29,10 @@ app.post('/sales', validateInputSale, salesController.createSale);
 
 app.put('/products/:id', productsController.updateById);
 
+app.put('/sales/:id', validateInputSale, salesController.updateById);
+
 app.delete('/products/:id', productsController.deleteById);
 
 app.delete('/sales/:id', salesController.deleteById);
 
-app.put('/sales/:id', validateInputSale, salesController.updateById);
-
-// não remova essa exportação, é para o avaliador funcionar
-// você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação
 module.exports = app;
